c3054ce69d01afc2209b0eacb83dc6dd4ff52bd7
Theo <theovincentjourdat@gmail.com>
2023-02-08 13:31:36 +0100

    count samples

--- experiments/car_on_hill/sample.py
+++ experiments/car_on_hill/sample.py
@@ -3,6 +3,7 @@ import argparse
 import json
 import jax
 import jax.numpy as jnp
+import numpy as np
 from tqdm import tqdm
 
 
@@ -26,10 +27,11 @@ def run_cli(argvs=sys.argv[1:]):
 
     from experiments.car_on_hill.utils import define_environment
     from idqn.sample_collection.replay_buffer import ReplayBuffer
+    from idqn.sample_collection.count_samples import count_samples
 
     sample_key = jax.random.PRNGKey(p["env_seed"])
 
-    env = define_environment(p["gamma"], p["n_states_x"], p["n_states_v"])[0]
+    env, _, states_x_boxes, _, states_v_boxes = define_environment(p["gamma"], p["n_states_x"], p["n_states_v"])
 
     replay_buffer = ReplayBuffer(p["n_samples"])
 
@@ -54,3 +56,13 @@ def run_cli(argvs=sys.argv[1:]):
     )
 
     replay_buffer.save(f"experiments/car_on_hill/figures/{args.experiment_name}/replay_buffer.npz")
+
+    samples_count, _, rewards_count = count_samples(
+        replay_buffer.states[:, 0],
+        replay_buffer.states[:, 1],
+        states_x_boxes,
+        states_v_boxes,
+        replay_buffer.rewards,
+    )
+    np.save(f"experiments/car_on_hill/figures/{args.experiment_name}/samples_count.npy", samples_count)
+    np.save(f"experiments/car_on_hill/figures/{args.experiment_name}/rewards_count.npy", rewards_count)
