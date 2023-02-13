0a659620f3b50933c7fb0c8613f78eba738ff522
Theo <theovincentjourdat@gmail.com>
2023-02-13 18:11:36 +0100

    scale bound weights

--- idqn/utils/importance_iteration.py
+++ idqn/utils/importance_iteration.py
@@ -3,9 +3,9 @@ import jax.numpy as jnp
 
 def importance_bound(gamma: float, n_bellman_iterations: int) -> jnp.ndarray:
     """
-    \alpha_k = (1 - \gamma) \gamma^{K-k-1} / (1 - \gamma^{K+1})
+    $\alpha_k = (1 - \gamma) \gamma^{K-k-1} / (1 - \gamma^{K+1})$
     so
-    \alpha_k = (1 - \gamma) \gamma^{K-1} / ((1 - \gamma^{K+1}) \gamma^{k})
+    $\alpha_k = (1 - \gamma) \gamma^{K-1} / ((1 - \gamma^{K+1}) \gamma^{k})$
     """
 
     pow_gammas = jnp.zeros(n_bellman_iterations)
@@ -14,4 +14,5 @@ def importance_bound(gamma: float, n_bellman_iterations: int) -> jnp.ndarray:
         pow_gammas = pow_gammas.at[idx].set(pow_gamma)
         pow_gamma *= gamma
 
-    return ((1 - gamma) * (pow_gamma / gamma)) / ((1 - pow_gamma * gamma) * pow_gammas)
+    # multiply by n_bellman_iterations to avoid changing the learning rate
+    return ((1 - gamma) * (pow_gamma / gamma)) / ((1 - pow_gamma * gamma) * pow_gammas) * n_bellman_iterations
