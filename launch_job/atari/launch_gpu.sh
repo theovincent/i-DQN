566b0fd6618f6a7a4e05b723cf86c70ff995522d
Theo <theovincentjourdat@gmail.com>
2023-03-07 10:03:24 +0100

    launcher

--- launch_job/atari/launch_gpu.sh
+++ launch_job/atari/launch_gpu.sh
@@ -17,23 +17,23 @@ for bellman_iterations_scope in "${LIST_BELLMAN_ITERATIONS_SCOPE[@]}"
 do
     # iDQN
     echo "launch train idqn 1/2"
-    submission_train_idqn_1=$(sbatch -J L_train_idqn_$EXPERIMENT_NAME --array=$FIRST_SEED-$LAST_SEED --cpus-per-task=3 --mem-per-cpu=1Gc --time=15:30:00 --output=out/atari/$EXPERIMENT_NAME/$bellman_iterations_scope\_train_idqn_%a.out --gres=gpu:1 -p amd,amd2,rtx,rtx2,dgx launch_job/atari/train_idqn.sh -e $EXPERIMENT_NAME -b $bellman_iterations_scope -g)
+    submission_train_idqn_1=$(sbatch -J L_train_idqn_$EXPERIMENT_NAME --array=$FIRST_SEED-$LAST_SEED --cpus-per-task=3 --mem-per-cpu=2Gc --time=3-00:00:00 --output=out/atari/$EXPERIMENT_NAME/$bellman_iterations_scope\_train_idqn_%a.out --gres=gpu:1 -p amd,amd2,rtx,rtx2,dgx launch_job/atari/train_idqn.sh -e $EXPERIMENT_NAME -b $bellman_iterations_scope -g)
 
     IFS=" " read -ra split_submission_train_idqn_1 <<< $submission_train_idqn_1
     submission_id_train_idqn_1=${split_submission_train_idqn_1[-1]}
 
     echo "launch evaluate idqn 1/2"
-    submission_evaluate_idqn_1=$(sbatch -J L_evaluate_idqn_$EXPERIMENT_NAME --dependency=after:$submission_id_train_idqn_1 --array=$FIRST_SEED-$LAST_SEED --cpus-per-task=1 --mem-per-cpu=2Gc --time=15:30:00 --output=out/atari/$EXPERIMENT_NAME/$bellman_iterations_scope\_evaluate_idqn_%a.out -p amd,amd2 launch_job/atari/evaluate_idqn.sh -e $EXPERIMENT_NAME -b $bellman_iterations_scope)
+    submission_evaluate_idqn_1=$(sbatch -J L_evaluate_idqn_$EXPERIMENT_NAME --dependency=after:$submission_id_train_idqn_1 --array=$FIRST_SEED-$LAST_SEED --cpus-per-task=1 --mem-per-cpu=2Gc --time=3-00:00:00 --output=out/atari/$EXPERIMENT_NAME/$bellman_iterations_scope\_evaluate_idqn_%a.out -p amd,amd2 launch_job/atari/evaluate_idqn.sh -e $EXPERIMENT_NAME -b $bellman_iterations_scope)
 
     IFS=" " read -ra split_submission_evaluate_idqn_1 <<< $submission_evaluate_idqn_1
     submission_id_evaluate_idqn_1=${split_submission_evaluate_idqn_1[-1]}
 
     echo "launch train idqn 2/2"
-    submission_train_idqn_2=$(sbatch -J L_train_idqn_$EXPERIMENT_NAME --dependency=afterok:$submission_id_train_idqn_1 --array=$FIRST_SEED-$LAST_SEED --cpus-per-task=3 --mem-per-cpu=1Gc --time=15:30:00 --output=out/atari/$EXPERIMENT_NAME/$bellman_iterations_scope\_train_idqn_%a.out --gres=gpu:1 -p amd,amd2,rtx,rtx2,dgx launch_job/atari/train_idqn.sh -e $EXPERIMENT_NAME -b $bellman_iterations_scope -r -g)
+    submission_train_idqn_2=$(sbatch -J L_train_idqn_$EXPERIMENT_NAME --dependency=afterok:$submission_id_train_idqn_1 --array=$FIRST_SEED-$LAST_SEED --cpus-per-task=3 --mem-per-cpu=2Gc --time=3-00:00:00 --output=out/atari/$EXPERIMENT_NAME/$bellman_iterations_scope\_train_idqn_%a.out --gres=gpu:1 -p amd,amd2,rtx,rtx2,dgx launch_job/atari/train_idqn.sh -e $EXPERIMENT_NAME -b $bellman_iterations_scope -r -g)
 
     IFS=" " read -ra split_submission_train_idqn_2 <<< $submission_train_idqn_2
     submission_id_train_idqn_2=${split_submission_train_idqn_2[-1]}
 
     echo "launch evaluate idqn 2/2"
-    submission_evaluate_idqn_2=$(sbatch -J L_evaluate_idqn_$EXPERIMENT_NAME --dependency=afterok:$submission_id_train_idqn_1,after:$submission_id_train_idqn_2 --array=$FIRST_SEED-$LAST_SEED --cpus-per-task=1 --mem-per-cpu=2Gc --time=15:30:00 --output=out/atari/$EXPERIMENT_NAME/$bellman_iterations_scope\_evaluate_idqn_%a.out -p amd,amd2 launch_job/atari/evaluate_idqn.sh -e $EXPERIMENT_NAME -b $bellman_iterations_scope -r)
+    submission_evaluate_idqn_2=$(sbatch -J L_evaluate_idqn_$EXPERIMENT_NAME --dependency=after:$submission_id_train_idqn_2 --array=$FIRST_SEED-$LAST_SEED --cpus-per-task=1 --mem-per-cpu=2Gc --time=3-00:00:00 --output=out/atari/$EXPERIMENT_NAME/$bellman_iterations_scope\_evaluate_idqn_%a.out -p amd,amd2 launch_job/atari/evaluate_idqn.sh -e $EXPERIMENT_NAME -b $bellman_iterations_scope -r)
 done
