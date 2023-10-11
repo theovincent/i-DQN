8b926874a8129a0bb2e3dfbd13dbb2b7eca137ee
Theo Vincent <theovincentjourdat@gmail.com>
2023-10-11 16:55:53 +0200

    dopamine requirements

--- launch_job/atari/launch_local_dqn.sh
+++ launch_job/atari/launch_local_dqn.sh
@@ -18,7 +18,7 @@ train_command="launch_job/atari/train_dqn.sh -e $EXPERIMENT_NAME -ns $N_PARALLEL
 
 # DQN
 echo "launch train dqn"
-if [[ $USE_DOCKER ]]
+if [[ $USE_DOCKER == true ]]
 then
     launch_job/docker_launcher.sh "$seed_command && $train_command"
 else
