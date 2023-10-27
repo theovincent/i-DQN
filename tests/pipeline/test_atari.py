229fd9b9fe6790b25d693737c51038a6a9b42650
Theo VINCENT <theovincentjourdat@gmail.con>
2023-10-27 12:47:58 +0200

    better test

--- tests/pipeline/test_atari.py
+++ tests/pipeline/test_atari.py
@@ -10,28 +10,33 @@ class TestPipeline(unittest.TestCase):
         super().__init__(*args, **kwargs)
         self.random_seed = np.random.randint(1000)
         print(f"random seed {self.random_seed}")
+        self.game = "Pong"
 
     def test_dqn_pipeline(self) -> None:
-        experiment_name = f"test_dqn"
+        experiment_name = "test_pipeline"
 
-        os.makedirs(f"experiments/atari/figures/{experiment_name}")
-        shutil.copyfile("tests/pipeline/parameters.json", f"experiments/atari/figures/{experiment_name}/")
+        if not os.path.exists(f"experiments/atari/figures/{experiment_name}"):
+            os.makedirs(f"experiments/atari/figures/{experiment_name}")
+        shutil.copyfile(
+            "tests/pipeline/parameters.json", f"experiments/atari/figures/{experiment_name}/parameters.json"
+        )
+
+        subprocess.run(["launch_job/create_tmux.sh"])
 
         output = subprocess.run(
             [
                 "launch_job/atari/launch_local_dqn.sh",
                 "-e",
-                f"{experiement_name}/{self.game}",
+                f"{experiment_name}/{self.game}",
                 "-fs",
-                self.random_seed,
+                str(self.random_seed),
                 "-ls",
-                self.random_seed,
+                str(self.random_seed),
                 "-ns",
-                1,
+                "1",
             ]
         )
 
-        os.remove(f"experiments/atari/figures/{experiment_name}/*")
-        os.remove(f"out/atari/{experiment_name}/*")
-
+        subprocess.run(["rm", "-r", f"experiments/atari/figures/{experiment_name}/{self.game}/DQN"])
+        subprocess.run(["rm", "-r", f"out/atari/{experiment_name}/{self.game}/train_dqn_{str(self.random_seed)}"])
         self.assertEqual(output, 0)
