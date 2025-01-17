c2082f345f42dce894b7b96a8ebb8b73c3f5630b
Théo VINCENT <49516344+theovincent@users.noreply.github.com>
2025-01-17 15:11:18 +0100

    local launch

--- launch_job/atari/launch.sh
+++ launch_job/atari/launch.sh
@@ -7,10 +7,18 @@
 
 # The index of the 5 seeds is 11, 21, 12, 22, 13.
 
-for GAME in Amidar Boxing Freeway Kangaroo MsPacman Pong SpaceInvaders Tennis
-do
-    launch_job/atari/launch_iiqn.sh --experiment_name hackatari/$GAME --first_seed 1 --last_seed 2 \
+# GAME from Amidar Boxing Freeway Kangaroo MsPacman Pong SpaceInvaders Tennis
+GAME=Amidar
+
+## Uncomment to launch seed 11 and seed 21
+# launch_job/atari/launch_iiqn.sh --experiment_name hackatari/$GAME --first_seed 1 --last_seed 1 \
         --list_bellman_iterations_scope 3 --n_parallel_seeds 2 
-    launch_job/atari/launch_iiqn.sh --experiment_name hackatari/$GAME --first_seed 3 --last_seed 3 \
+
+## Uncomment to launch seed 12 and seed 22
+# launch_job/atari/launch_iiqn.sh --experiment_name hackatari/$GAME --first_seed 2 --last_seed 2 \
+        --list_bellman_iterations_scope 3 --n_parallel_seeds 2
+
+## Uncomment to launch seed 13
+# launch_job/atari/launch_iiqn.sh --experiment_name hackatari/$GAME --first_seed 3 --last_seed 3 \
         --list_bellman_iterations_scope 3 --n_parallel_seeds 1
-done
+
