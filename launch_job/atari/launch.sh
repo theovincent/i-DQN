9a4ea34c7a228001122c5d9697e6b45d34c1c07a
Theo Vincent <theovincentjourdat@gmail.com>
2025-01-20 12:06:12 +0100

    customize launcher

--- launch_job/atari/launch.sh
+++ launch_job/atari/launch.sh
@@ -5,20 +5,26 @@
 # L_11.npy is the loss for each gradient step.
 # Q_11_$N_BEST_EPOCH\_best_online_params in the weights of the best online network seen so far.
 
-# The index of the 5 seeds is 11, 21, 12, 22, 13.
+# ---- The logs will be dump in out/hackatari/$GAME/ ---- #
 
 # GAME from Amidar Boxing Freeway Kangaroo MsPacman Pong SpaceInvaders Tennis
 GAME=Amidar
 
-## Uncomment to launch seed 11 and seed 21
-# launch_job/atari/launch_iiqn.sh --experiment_name hackatari/$GAME --first_seed 1 --last_seed 1 \
+# SEED from 1 2 3 ---- The indices of the 5 seeds are 11, 21, 12, 22, 13.
+SEED=1
+
+## Launches seed 11 and seed 21
+if [[ $SEED == 1 ]]
+then
+    launch_job/atari/launch_local_iiqn.sh --experiment_name hackatari/$GAME --first_seed 1 --last_seed 1 \
         --list_bellman_iterations_scope 3 --n_parallel_seeds 2 
-
-## Uncomment to launch seed 12 and seed 22
-# launch_job/atari/launch_iiqn.sh --experiment_name hackatari/$GAME --first_seed 2 --last_seed 2 \
+## Launches seed 12 and seed 22
+elif [[ $SEED == 2 ]]
+then
+    launch_job/atari/launch_local_iiqn.sh --experiment_name hackatari/$GAME --first_seed 2 --last_seed 2 \
         --list_bellman_iterations_scope 3 --n_parallel_seeds 2
-
-## Uncomment to launch seed 13
-# launch_job/atari/launch_iiqn.sh --experiment_name hackatari/$GAME --first_seed 3 --last_seed 3 \
+## Launches seed 13
+else
+    launch_job/atari/launch_local_iiqn.sh --experiment_name hackatari/$GAME --first_seed 3 --last_seed 3 \
         --list_bellman_iterations_scope 3 --n_parallel_seeds 1
-
+fi
--- launch_job/atari/launch_local_iiqn.sh
+++ launch_job/atari/launch_local_iiqn.sh
@@ -12,6 +12,10 @@ EXPERIMENT_GENERAL_NAME=${split_experiment_name[0]}
 [ -f experiments/atari/figures/$EXPERIMENT_GENERAL_NAME/parameters.json ] || cp experiments/atari/parameters.json experiments/atari/figures/$EXPERIMENT_GENERAL_NAME/parameters.json
 [ -d experiments/atari/figures/$EXPERIMENT_NAME/iIQN ] || mkdir experiments/atari/figures/$EXPERIMENT_NAME/iIQN
 
+if ! tmux has-session -t hackatari; then
+    tmux new-session -d -s hackatari
+    echo "Created new tmux session - hackatari"
+fi
 
 seed_command="export SLURM_ARRAY_TASK_ID=$FIRST_SEED"
 train_command="launch_job/atari/train_iiqn.sh -e $EXPERIMENT_NAME -b ${LIST_BELLMAN_ITERATIONS_SCOPE[0]} -ns $N_PARALLEL_SEEDS"
@@ -22,5 +26,5 @@ if [[ $USE_DOCKER == true ]]
 then
     launch_job/docker_launcher.sh "$seed_command && $train_command"
 else
-    tmux send-keys -t train "$seed_command" ENTER "$train_command" ENTER
+    tmux send-keys -t hackatari "$seed_command" ENTER "$train_command" ENTER
 fi
