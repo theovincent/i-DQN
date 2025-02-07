b29f8701cc5da7af15a73f002b290188144acc88
kevingerhardt09@t-online.de <kevingerhardt09@t-online.de>
2025-02-07 14:25:50 +0100

    fix lunar lander idqn launch script

--- launch_job/lunar_lander/local_idqn.sh
+++ launch_job/lunar_lander/local_idqn.sh
@@ -21,7 +21,7 @@ fi
 echo "launch train $ALGO_NAME local"
 for (( seed=$FIRST_SEED; seed<=$LAST_SEED; seed++ ))
 do
-    tmux send-keys -t slimdqn\
+    tmux send-keys -t slimidqn\
     "python3 experiments/$ENV_NAME/$ALGO_NAME.py --experiment_name $EXPERIMENT_NAME --seed $seed $ARGS >> experiments/$ENV_NAME/logs/$EXPERIMENT_NAME/$ALGO_NAME/seed_$seed.out 2>&1 &" ENTER
 done
-tmux send-keys -t slimdqn "wait" ENTER
+tmux send-keys -t slimidqn "wait" ENTER
