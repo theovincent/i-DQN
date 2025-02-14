e78dfd7c5706548e0cfde03fbea3e6b9175989f7
kevingerhardt09@t-online.de <kevingerhardt09@t-online.de>
2025-02-14 11:08:32 +0100

    updated idqn

--- slim_idqn/networks/idqn.py
+++ slim_idqn/networks/idqn.py
@@ -6,8 +6,8 @@ import numpy as np
 import optax
 from flax.core import FrozenDict
 
-from slimdqn.networks.architectures.dqn import DQNNet
-from slimdqn.sample_collection.replay_buffer import ReplayBuffer, ReplayElement
+from slim_idqn.networks.architectures.dqn import DQNNet
+from slim_idqn.sample_collection.replay_buffer import ReplayBuffer, ReplayElement
 
 
 class iDQN:
@@ -28,15 +28,14 @@ class iDQN:
         num_networks: int = 5
     ):
         keys = jax.random.split(key, num=num_networks+1)
-
         self.num_networks = num_networks
         self.network = DQNNet(features, architecture_type, n_actions)
-        self.online_params = jax.vmap(self.network.init, in_axes=(0, None))(keys[1::], jnp.zeros(observation_dim, dtype=jnp.float32))
-        self.target_params = jnp.roll(self.online_params, 1)
-        self.target_params = self.target_params.at[0].set(self.network.init(keys[0], jnp.zeros(observation_dim, dtype=jnp.float32)))
 
-        self.optimizer = self.num_networks, optax.adam(learning_rate, eps=adam_eps)
-        self.optimizer_state = self.optimizer.init(self.online_params[0])
+        self.online_params_params = jax.vmap(self.network.init, in_axes=(0, None))(keys, jnp.zeros(observation_dim, dtype=jnp.float32))
+        self.target_params = self.online_params
+
+        self.optimizer = optax.adam(learning_rate, eps=adam_eps)
+        self.optimizer_state = self.optimizer.init(self.network.init(keys[0], jnp.zeros(observation_dim, dtype=jnp.float32)))
 
         self.gamma = gamma
         self.update_horizon = update_horizon
@@ -49,39 +48,31 @@ class iDQN:
         if step % self.update_to_data == 0:
             batch_samples = replay_buffer.sample()
 
-            self.online_params, self.optimizer_states, losses = jax.vmap(self.single_learn_on_batch, in_axes=(0, 0, 0, None))(
+            self.online_params, self.optimizer_state, loss = self.learn_on_batch(
                 self.online_params,
                 self.target_params,
-                self.optimizer_states,
+                self.optimizer_state,
                 batch_samples
             )
-            loss = losses[-1]
-
-            #for k in range(self.num_networks):
-            #    self.online_params[k], self.optimizer_state, loss = self.single_learn_on_batch(
-            #        self.online_params[k],
-            #        self.target_params[k],
-            #        self.optimizer_state,
-            #        batch_samples
-            #    )
-
             self.cumulated_loss += loss
+
 
     def update_target_params(self, step: int):
         if step % self.target_update_frequency == 0:
-            for k in range(1, self.num_networks):
-                self.target_params[k] = self.online_params[k-1].copy()
-                
+            self.target_params = self.online_params
+
             logs = {"loss": self.cumulated_loss / (self.target_update_frequency / self.update_to_data)}
             self.cumulated_loss = 0
 
             return True, logs
         return False, {}
+    
+    def roll(self, params):
+        return jax.tree_util.tree_map(lambda param: param.at[:-1].set(param[1:]), params)
     
     def shift_params(self, step: int):
         if step % self.shift_params_frequency == 0:
-            for k in range(0, self.num_networks):
-                self.target_params[k] = self.online_params[k].copy()
+            self.target_params = self.roll(self.online_params)
 
             logs = {"loss": self.cumulated_loss / (self.target_update_frequency / self.update_to_data)}
             self.cumulated_loss = 0
@@ -90,36 +81,41 @@ class iDQN:
         return False, {}
     
 
-
-
     @partial(jax.jit, static_argnames="self")
-    def single_learn_on_batch(
+    def learn_on_batch(
         self,
         params: FrozenDict,
         params_target: FrozenDict,
         optimizer_state,
         batch_samples,
     ):
-        loss, grad_loss = jax.value_and_grad(self.loss_on_batch)(params, params_target, batch_samples)
-        updates, optimizer_state = self.optimizer.update(grad_loss, optimizer_state)
+        losses, grad_losses = jax.value_and_grad(self.loss_on_batch)(params, params_target, batch_samples)
+        updates, optimizer_state = self.optimizer.update(grad_losses, optimizer_state)  
         params = optax.apply_updates(params, updates)
 
-        return params, optimizer_state, loss
+        return params, optimizer_state, losses
 
     def loss_on_batch(self, params: FrozenDict, params_target: FrozenDict, samples):
-        return jax.vmap(self.loss, in_axes=(None, None, 0))(params, params_target, samples).mean()
+        losses =  jax.vmap(self.loss, in_axes=(None, None, 0))(params, params_target, samples)
+
+        return losses.mean()
 
     def loss(self, params: FrozenDict, params_target: FrozenDict, sample: ReplayElement):
         # computes the loss for a single sample
         target = self.compute_target(params_target, sample)
-        q_value = self.network.apply(params, sample.state)[sample.action]
-        return jnp.square(q_value - target)
+        q_value = self.apply_other_heads(params, sample.state)
 
-    def compute_target(self, params: FrozenDict, sample: ReplayElement):
-        # computes the target value for single sample
-        return sample.reward + (1 - sample.is_terminal) * (self.gamma**self.update_horizon) * jnp.max(
-            self.network.apply(params, sample.next_state)
-        )
+        return jnp.square(q_value - target)
+    
+    def apply_other_heads(self, params, state):
+        remaining_params = jax.tree_util.tree_map(lambda param: param[1:], params)
+
+        return jax.vmap(self.network.apply, in_axis=(0, None))(remaining_params, state)
+
+    def compute_target(self, params, sample: ReplayElement):
+        max_next_q = jnp.max(jax.vmap(self.network.apply, in_axis=(0, None))(params, sample.next_state)[:-1], axis=0)
+
+        return sample.reward + (1 - sample.is_terminal) * (self.gamma**self.update_horizon) * max_next_q
 
     @partial(jax.jit, static_argnames="self")
     def best_action(self, params: FrozenDict, state: jnp.ndarray):
